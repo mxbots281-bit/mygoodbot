ed03cb28329178732cb18078a95d5a97ecc7771c
mxbots281-bit <mxbots281@gmail.com>
2026-08-12 19:30:32 +0300

    main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ from telethon.tl.functions.account import ReportPeerRequest
 from telethon import types
 
 # ─── Configuration ───────────────────────────────────────────────────────
-BOT_TOKEN = "8937034293:AAFoR3Jq0Kw3_6ZezI6W00fGz_qHyieCDPQ"
+BOT_TOKEN = 8977749945:AAHLwwmKowJ4bukF-SM4rSXH2YVj3d6-0R0
 OWNER_ID = 6019063884
 DB_FILE = "panel_bot.db"
 
